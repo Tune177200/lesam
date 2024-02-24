59372113ebacf038cc8278546ca40d2a8b1f1412
vubinh12720 <binhvl@newwave.vn>
2024-02-24 20:28:20 +0700

    contact page

--- wp-content/themes/lesam/template/contact.php
+++ wp-content/themes/lesam/template/contact.php
@@ -10,10 +10,46 @@
             <div class="contact-page">
                 <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2">
                     <div class="col">
-                        1
+                        <div class="detail_company">
+                            <div class="logo">
+                                <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/happynut-logo.png' ?>" alt="Happynut">
+                            </div>  
+                            <h3>CÔNG TY TNHH HAPPY NUTS VIỆT NAM</h3>
+                            <p>Mọi thắc mắc quý khách vui lòng liên hệ tới chúng tôi thông qua thông tin bên dưới hoặc quý khách có thể điền thông tin ở form bên cạnh. Chúng tôi sẽ liên hệ giải đáp thắc mắc tới quý khách một cách sớm nhất.</p>
+                            <div class="d-flex">
+                                <div>
+                                    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13.3342 14.2139L15.0564 12.491C15.2883 12.2618 15.5818 12.1049 15.9012 12.0394C16.2205 11.9739 16.552 12.0025 16.8555 12.1218L18.9544 12.9602C19.261 13.0847 19.5239 13.2972 19.71 13.5709C19.8961 13.8447 19.997 14.1674 20 14.4985V18.3442C19.9982 18.5694 19.9509 18.7919 19.8609 18.9983C19.7709 19.2047 19.6401 19.3908 19.4763 19.5453C19.3125 19.6998 19.1192 19.8195 18.9079 19.8973C18.6967 19.9751 18.4719 20.0093 18.2471 19.9979C3.5392 19.0826 0.57149 6.62239 0.010239 1.85368C-0.0158147 1.6195 0.00798984 1.38246 0.0800864 1.15814C0.152183 0.933831 0.270938 0.727331 0.428538 0.55223C0.586138 0.377128 0.779012 0.237394 0.994471 0.142219C1.20993 0.0470438 1.44309 -0.00141458 1.67862 3.14343e-05H5.3921C5.72347 0.0010127 6.04698 0.101149 6.321 0.287561C6.59503 0.473973 6.80704 0.738135 6.92977 1.04607L7.7678 3.14584C7.89101 3.44817 7.92245 3.78011 7.85818 4.10021C7.79391 4.4203 7.63679 4.71436 7.40645 4.94565L5.68426 6.66854C5.68426 6.66854 6.67606 13.3832 13.3342 14.2139Z" fill="#CCCCCC"></path>
+                                    </svg>
+                                </div>
+                                <p class="ms-4">
+                                    <a href="tel:(+84) 985 023 463">(+84) 985 023 463</a>
+                                </p>
+                            </div>
+                            <div class="d-flex">
+                                <div>
+                                    <svg width="24" height="17" viewBox="0 0 24 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M21.6 0H2.4C1.08 0 0.012 0.95625 0.012 2.125L0 14.875C0 16.0438 1.08 17 2.4 17H21.6C22.92 17 24 16.0438 24 14.875V2.125C24 0.95625 22.92 0 21.6 0ZM21.6 4.25L12 9.5625L2.4 4.25V2.125L12 7.4375L21.6 2.125V4.25Z" fill="#CCCCCC"></path>
+                                    </svg>
+                                </div>
+                                <p class="ms-4">
+                                    <a href="mailto:info@happynuts.vn">info@happynuts.vn</a>
+                                </p>
+                            </div>
+                            <div class="d-flex">
+                                <div>
+                                    <svg width="18" height="23" viewBox="0 0 18 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M9 0C11.3869 0 13.6761 1.03852 15.364 2.88709C17.0518 4.73566 18 7.24287 18 9.85714C18 13.938 15.16 18.2138 9.6 22.781C9.4269 22.9231 9.21637 23 9 23C8.78363 23 8.5731 22.9231 8.4 22.781C2.84 18.2138 0 13.938 0 9.85714C0 7.24287 0.948211 4.73566 2.63604 2.88709C4.32387 1.03852 6.61305 0 9 0ZM9 6.57143C8.20435 6.57143 7.44129 6.9176 6.87868 7.53379C6.31607 8.14998 6 8.98572 6 9.85714C6 10.7286 6.31607 11.5643 6.87868 12.1805C7.44129 12.7967 8.20435 13.1429 9 13.1429C9.79565 13.1429 10.5587 12.7967 11.1213 12.1805C11.6839 11.5643 12 10.7286 12 9.85714C12 8.98572 11.6839 8.14998 11.1213 7.53379C10.5587 6.9176 9.79565 6.57143 9 6.57143Z" fill="#CCCCCC"></path>
+                                    </svg>
+                                </div>
+                                <p class="ms-4">72 đường số 11, KDC Cityland Park Hills, <br>Phường 10, Quận Gò Vấp, Tp. Hồ Chí Minh</p>
+                            </div>
+                        </div>
                     </div>
                     <div class="col">
-                        <?php echo do_shortcode('[contact-form-7 id="2de348b" title="Liên Hệ"]'); ?>
+                        <div class="contact_form">
+                            <?php echo do_shortcode('[contact-form-7 id="2de348b" title="Liên Hệ"]'); ?>
+                        </div>
                     </div>
                 </div>
             </div>
